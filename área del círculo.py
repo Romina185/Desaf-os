66e24f3261525c21ee66825438efd119155c37b3
Romina185 <racosta2@docente.ceibal.edu.uy>
2025-05-28 20:12:33 -0300

    Add files via upload

---
+++ área del círculo.py
@@ -0,0 +1,10 @@
+def calcular_area_circulo(radio):
+ #solicitar radio
+ radio=4
+#solicitar PI
+PI:3.14159
+#Calcular el area
+area =("math.pi *radio **2")
+area =(3.14159*4**2)
+#Imprimir el resultado
+print(area)
