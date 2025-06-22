a6b44c4f44eb66abfe15a3c464bfd46c99ce44bb
Romina185 <racosta2@docente.ceibal.edu.uy>
2025-06-22 11:16:40 -0300

    Add files via upload

---
+++ Ordenar la lista.py
@@ -0,0 +1,8 @@
+#Se define ordenar la lista
+def ordenar_lista_en_sitio(lista):
+    lista.sort()
+    return lista
+#Seleccionar los números 
+numeros = [9, 6, 12, 19, 15, 11]
+ordenar_lista_en_sitio(numeros)
+print("Lista ordenada:", numeros)
